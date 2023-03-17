ed7fde6f41fe55560424e582b69a3916ac805ac3
Rajat Guptat <rajatgupta.tech@gmail.com>
2023-03-17 18:22:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Logo from "./Images/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+        <div className='main'>
+            <div className='navbar'>
+                <div>
+                    <img src={Logo}></img>
+                    <span className='heading'>Ad Astra</span>
+                </div>
+                <div className='navbar1'>
+                    <span className='text'>Home</span>
+                    <span className='text'>Store</span>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </>
+  )
 }
 
-export default App;
+export default App
